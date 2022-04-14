ce4acaed6982699d763c8820fd8f6fb77a776463
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-04-14 16:42:44 +0100

    MASTER: Finish large problem B implementation.

--- Large problemas (pair)/Problem B - A pyramid scheme/src/Main.java
+++ Large problemas (pair)/Problem B - A pyramid scheme/src/Main.java
@@ -1,90 +1,84 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    public class Member {
-        private int id;
-        private int value;
-        private int[] recruitedMembers;
-        private int nRecruitedMembers;
+    public static class Member {
+        public int[] contacts;
+        public int nContacts;
         public final int MAX_RECRUITED_MEMBERS = 10;
 
         public Member() {
-            recruitedMembers = new int[MAX_RECRUITED_MEMBERS];
-            nRecruitedMembers = 0;
+            contacts = new int[MAX_RECRUITED_MEMBERS + 1];
+            nContacts = 0;
         }
 
-        public Member(int id) {
-            this();
-            this.id = id;
-
-        }
-
-        public Member(int id, int value) {
-            this();
-            this.id = id;
-            this.value = value;
-        }
-
-        public void addRecruitedMember(int memberId) {
-            if (nRecruitedMembers < MAX_RECRUITED_MEMBERS) {
-                recruitedMembers[nRecruitedMembers++] = memberId;
+        public void addContact(int memberId) {
+            if (nContacts < MAX_RECRUITED_MEMBERS + 1) {
+                contacts[nContacts++] = memberId;
             }
         }
+    }
 
-        public int getId() {
-            return id;
-        }
+    public static class Solution {
+        public int nMembers;
+        public int value;
 
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
-
-        public int[] getRecruitedMembers() {
-            return recruitedMembers;
-        }
-
-        public void setRecruitedMembers(int[] recruitedMembers) {
-            this.recruitedMembers = recruitedMembers;
-        }
-
-        public int getNRecruitedMembers() {
-            return nRecruitedMembers;
-        }
-
-        public void setNRecruitedMembers(int nRecruitedMembers) {
-            this.nRecruitedMembers = nRecruitedMembers;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            return getId() == ((Member) o).getId();
-        }
-
-        @Override
-        public int hashCode() {
-            return getId();
+        public Solution best(Solution other) {
+            if (this.nMembers < other.nMembers) {
+                return this;
+            } else if (this.nMembers > other.nMembers) {
+                return other;
+            } else {
+                if (this.value > other.value) {
+                    return this;
+                } else {
+                    return other;
+                }
+            }
         }
     }
 
-    public class Solution {
-        private HashSet<Member> members;
-        private int value;
+    private final int MAX_MEMBERS = 100001;
+    private final int ROOT_MEMBER_PARENT = -1;
+    private final String EOF = "-1";
+
+    private Member[] pyramid;
+    private Solution[][] memo;
+
+    private void solve_(int memberId, int recruiterId) {
+        int currContact;
+        Member member;
+
+        member = pyramid[memberId];
+        for (int i = 0; i < member.nContacts; i++) {
+            currContact = member.contacts[i];
+            if (currContact != recruiterId)
+                solve_(currContact, memberId);
+        }
+
+        for (int i = 0; i < member.nContacts; i++) {
+            currContact = member.contacts[i];
+
+            if (member.contacts[i] != recruiterId) {
+                memo[memberId][0].nMembers += memo[currContact][1].nMembers;
+                memo[memberId][0].value += memo[currContact][1].value;
+
+                Solution best = memo[currContact][1].best(memo[currContact][0]);
+
+                memo[memberId][1].nMembers += best.nMembers;
+                memo[memberId][1].value += best.value;
+            }
+        }
     }
 
-    private static int MAX_MEMBERS = 100001;
-    private static String EOF = "-1";
+    private void solve() {
+        solve_(0, ROOT_MEMBER_PARENT);
+        Solution best = memo[0][0].best(memo[0][1]);
+
+        System.out.println(best.nMembers + " " + best.value);
+    }
 
     public Main() throws IOException {
         int idRecruiter;
@@ -93,22 +87,30 @@ public class Main {
         String token;
         String line;
         Member recruiter;
-        Member[] pyramid;
         BufferedReader in;
         StringTokenizer st;
 
         in = new BufferedReader(new InputStreamReader(System.in));
 
         while ((line = in.readLine()) != null && line.length() > 0) {
+            pyramid = new Member[MAX_MEMBERS];
+            memo = new Solution[MAX_MEMBERS][2];
+
             while (!line.equals(EOF)) {
-                pyramid = new Member[MAX_MEMBERS];
                 st = new StringTokenizer(line);
                 idRecruiter = Integer.parseInt(st.nextToken());
 
                 if (pyramid[idRecruiter] == null) {
-                    pyramid[idRecruiter] = new Member(idRecruiter);
+                    pyramid[idRecruiter] = new Member();
                 }
 
+                if (memo[idRecruiter][0] == null) {
+                    memo[idRecruiter][0] = new Solution();
+                    memo[idRecruiter][1] = new Solution();
+                }
+
+                memo[idRecruiter][0].nMembers = 0;
+                memo[idRecruiter][1].nMembers = 1;
                 recruiter = pyramid[idRecruiter];
 
                 while (true) {
@@ -119,15 +121,23 @@ public class Main {
                     }
 
                     idRecruited = Integer.parseInt(token);
-                    pyramid[idRecruited] = new Member(idRecruited);
-                    recruiter.addRecruitedMember(idRecruited);
+
+                    if (pyramid[idRecruited] == null) {
+                        pyramid[idRecruited] = new Member();
+                    }
+
+                    pyramid[idRecruited].addContact(idRecruiter);
+                    recruiter.addContact(idRecruited);
                 }
 
                 value = Integer.parseInt(token);
-                recruiter.setValue(value);
+                memo[idRecruiter][0].value = 0;
+                memo[idRecruiter][1].value = value;
 
                 line = in.readLine();
             }
+
+            solve();
         }
     }
 
